47d05427b20747b444aafe3a2d0b2f5830b596bc
Tom Britton <tom.jf.britton@gmail.com>
2022-08-31 15:42:50 +0100

    Done some of the tests

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,26 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.getFilmTitles = function(){
+  let titles = this.films.map((film) => {
+    return film.title
+  });
+  return titles
+};
+
+Cinema.prototype.getFilmByGenre = function(genre){
+  let filmsByGenre = this.films.filter((film) => {
+    return film.genre === genre
+  });
+  return filmsByGenre
+};
+
+Cinema.prototype.getFilmByTitle = function(title){
+  let filmsOfTitle = this.films.filter((film) => {
+    return film.title === title
+  });
+  return filmsOfTitle
+};
+
+
 module.exports = Cinema;
